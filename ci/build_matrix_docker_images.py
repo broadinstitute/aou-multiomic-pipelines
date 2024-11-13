192e98356e4681e418aa34ba64d8e31e8d1381f5
Lucas van Dijk <info@lucasvandijk.nl>
2024-11-13 16:41:24 -0500

    feat: Add script that lists Docker imgs from a file list
    To be used with the `changed-files` Github action. Obtain
    the list of docker images to rebuild based on a list of changed
    files.

---
+++ ci/build_matrix_docker_images.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Parse a list of changed files since last commit,
+and determine the list of Docker images to build.
+"""
+
+import sys
+import json
+import fileinput
+from pathlib import Path
+
+# Main directory holding all docker images to build. The directory
+# should be relative to the repository root.
+#
+# This scripts assumes that each docker image to build is in
+# its own subdirectory of the listed folder below.
+DOCKER_IMGS_PATH = Path("docker")
+
+# Google Artifact Registry repository URL
+GAR_REPOSITORY = "us-central1-a.pkg.dev/broad-dsp-lrma/aou-multiomics/"
+
+
+def find_repo_root(fname: Path):
+    if fname.is_file():
+        fname = fname.parent
+
+    while fname != Path('/'):
+        has_git = (fname / ".git").is_dir()
+
+        if has_git:
+            return fname
+
+        fname = fname.parent
+
+
+def find_docker_root(fname):
+    """Finds the first parent folder of `fname` that contains a `Dockerfile`."""
+
+    if fname.name == 'Dockerfile':
+        return fname.parent
+
+    curr_dir: Path = fname.parent
+    while curr_dir != Path('/'):
+        has_dockerfile = (curr_dir / 'Dockerfile').is_file()
+
+        if has_dockerfile:
+            return curr_dir
+
+        curr_dir = curr_dir.parent
+
+
+def main():
+    repo_root = find_repo_root(Path(__file__))
+    repo_docker_root = repo_root / DOCKER_IMGS_PATH
+
+    docker_images = set()
+    for f in map(lambda line: Path(line.strip()), fileinput.input()):
+        if not f.is_file():
+            print("Non-existing file:", f, file=sys.stderr)
+            continue
+
+        docker_root = find_docker_root(f.resolve())
+
+        if docker_root.is_relative_to(repo_docker_root):
+            docker_img_name = docker_root.name
+            docker_images.add(docker_img_name)
+
+    output = []
+    for img_name in docker_images:
+        output.append({
+            "dockerfile": (repo_docker_root / img_name / "Dockerfile").as_posix(),
+            "img_name": GAR_REPOSITORY + img_name
+        })
+
+    print(json.dumps(output))
+
+
+if __name__ == '__main__':
+    main()
